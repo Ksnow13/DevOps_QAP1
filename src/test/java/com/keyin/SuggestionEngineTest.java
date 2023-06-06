14ead1dd3cc4e0e6304e1ed6ecb9b4eedfaee096
Ksnow <107448069+Ksnow13@users.noreply.github.com>
2023-06-05 22:47:45 -0230

    Added comments

--- src/test/java/com/keyin/SuggestionEngineTest.java
+++ src/test/java/com/keyin/SuggestionEngineTest.java
@@ -1,3 +1,13 @@
+/*
+
+Kyle Snow
+Software Development Semester 4
+Software Design, Architecture, Testing & DevOps
+Started  June 1st, 2023
+Finished June 5th, 2023
+
+ */
+
 package com.keyin;
 
 import com.keyin.SuggestionEngine;
@@ -20,8 +30,6 @@ public class SuggestionEngineTest {
     @DisplayName("Test Suggestion Engine With Given Word")
     public void testSuggestion() throws IOException, URISyntaxException {
 
-        //SuggestionEngine suggestionEngine = new SuggestionEngine();
-
         URI uri =  ClassLoader.getSystemResource("words.txt").toURI();
         String mainPath = Paths.get(uri).toString();
         Path path = Paths.get(mainPath);
@@ -34,7 +42,6 @@ public class SuggestionEngineTest {
     @Test
     @DisplayName("Test Suggestion Engine Failure")
     public void testSuggestionFail() throws IOException, URISyntaxException {
-       //SuggestionEngine suggestionEngine = new SuggestionEngine();
         URI uri =  ClassLoader.getSystemResource("words.txt").toURI();
         String mainPath = Paths.get(uri).toString();
         Path path = Paths.get(mainPath);
@@ -44,10 +51,11 @@ public class SuggestionEngineTest {
         Assertions.assertFalse(suggestionEngine.generateSuggestions("class").contains("class"));
     }
 
+    // SuggestionEngine had a bug where it would not find suggestions when given a word with a capital letter
+    // The bug has been fixed and this test method makes sure it is working.
     @Test
     @DisplayName("Test Suggestion Engine With Capital Words")
     public void testSuggestionCaseSensitive() throws IOException, URISyntaxException {
-        //SuggestionEngine suggestionEngine = new SuggestionEngine();
         URI uri =  ClassLoader.getSystemResource("words.txt").toURI();
         String mainPath = Paths.get(uri).toString();
         Path path = Paths.get(mainPath);
@@ -57,10 +65,11 @@ public class SuggestionEngineTest {
         Assertions.assertTrue(suggestionEngine.generateSuggestions("Hell-").contains("hello"));
     }
 
+    // testing if the loadDictionary method successfully loads the words.txt file data
+    // if loadDictionary method works it returns true, else it returns false
     @Test
     @DisplayName("Test If Dictionary Data Loads")
     public void testLoadDictionaryData() throws URISyntaxException, IOException {
-
         URI uri =  ClassLoader.getSystemResource("words.txt").toURI();
         String mainPath = Paths.get(uri).toString();
         Path path = Paths.get(mainPath);
